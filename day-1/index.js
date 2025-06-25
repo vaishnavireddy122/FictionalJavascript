6b18ad4fc73973802e58c4a30b123180b3303b9f
vaishnavireddy122 <153925403+vaishnavireddy122@users.noreply.github.com>
2025-06-25 14:18:09 +0530

    Update index.js

--- day-1/index.js
+++ day-1/index.js
@@ -1,1 +1,38 @@
 console.log("This is day-1 JS file");
+//TERNARY OPERATOR
+//EXAMPLE-1
+
+let x=10;
+let y=x ? "3":"6";
+console.log(y);
+
+
+//EXAMPLE-2
+isLoggedIn=false
+const message = isLoggedIn ? "Welcome back!" : "Please log in.";
+console.log(message)
+
+//EXAMPLE-3 
+let isAdmin = false;
+isAdmin ? grantAccess() : denyAccess()
+function grantAccess()
+{
+  console.log("Granted Accesss!!");
+}
+function denyAccess()
+{
+    console.log("Access Denied!!");
+}
+
+
+//NESTED TERNARY
+
+let a=10;
+let b=15;
+let c=a>b ?(a>0 ? "a is greater than b":"a is 0")
+          : "a is smaller than b";
+console.log(c);
+
+
+
+
